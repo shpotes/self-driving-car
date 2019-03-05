4fd1ca6bb211abf73da88d6674bd050368771eb3
Santiago Hincapie <shinca12@eafit.edu.co>
2019-03-05 01:45:09 -0500

    Update model.sh

--- experiments/raspi/tf-lite/lite/model.sh
+++ experiments/raspi/tf-lite/lite/model.sh
@@ -1,6 +1,6 @@
 rm -rf model
 mkdir model
 cd model
-wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/model_zoo/upload_20180427/inception_resnet_v2_2018_04_27.tgz
-tar xvf inception_resnet_v2_2018_04_27.tgz
-rm inception_resnet_v2_2018_04_27.tgz
+wget http://download.tensorflow.org/models/inception_v2_224_quant_20181026.tgz
+tar xvf inception_v2_224_quant_20181026.tgz
+rm inception_v2_224_quant_20181026.tgz
