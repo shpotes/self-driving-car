421e341b6bf8fef0f9705d5e2710889de0de559b
Juan Fernando Ossa <jossava@eafit.edu.co>
2019-03-09 00:38:34 -0500

    streaming

--- streaming.py
+++ streaming.py
@@ -1,17 +1,18 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('http://192.168.0.106:8080/?action=stream')
+#cap = cv2.VideoCapture('http://192.168.0.106:8080/?action=stream')
+cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
